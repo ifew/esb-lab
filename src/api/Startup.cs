4d739d37ea110c235f3827ba5927a6188996467e
iFew <chitpong@gmail.com>
2018-02-08 15:19:45 +0700

    Added Indent

--- src/api/Startup.cs
+++ src/api/Startup.cs
@@ -69,8 +69,8 @@ namespace api
                 c.IncludeXmlComments(xmlPath);
             });
 
-                services.AddMvc();
-            }
+            services.AddMvc();
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
